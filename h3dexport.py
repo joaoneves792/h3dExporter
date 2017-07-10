d1cf1c18c1ef1e402dc49b27bdebf953c9678e39
Joao Neves <joaoneves792@gmail.com>
2017-07-10 23:19:55 +0100

    Exporting keyframes

--- h3dexport.py
+++ h3dexport.py
@@ -3,6 +3,20 @@ import struct
 from mathutils import Matrix
 from math import pi
 
+###############################################
+#Extend Object with a is_keyframe method
+def is_keyframe(ob, frame, data_path, array_index=-1):
+    if ob is not None and ob.animation_data is not None and ob.animation_data.action is not None:
+        for fcu in ob.animation_data.action.fcurves:
+            if fcu.data_path == data_path:
+                if array_index == -1 or fcu.array_index == array_index:
+                    return frame in (p.co.x for p in fcu.keyframe_points)
+    return False
+
+bpy.types.Object.is_keyframe = is_keyframe
+###############################################
+
+
 def binary_write_string(f, string):
     count = len(string)
     f.write(struct.pack("<b", count))
@@ -34,6 +48,12 @@ class H3dMesh:
         self.blender_armature = None
         self.h3d_armature = None
 
+class H3dKeyframe:
+    def __init__(self):
+        self.frame = 0
+        self.rotation = [0, 0, 0]
+        self.position = [0, 0, 0]
+
 class H3dJoint:
     def __init__(self):
         self.name = None
@@ -43,6 +63,7 @@ class H3dJoint:
         self.rotation = [0,0,0] #Euler
         self.position = [0,0,0]
         self.index = 0
+        self.keyframes = [] #These must be sorted according to frame number
 
 class H3dArmature:
     def __init__(self):
@@ -50,6 +71,50 @@ class H3dArmature:
         self.blender_armature = None
         self.joints_dic = {}
         self.joints = []
+
+def fill_keyframes(scene, h3d_armature):
+    base_bone_correction = Matrix.Rotation(pi / 2, 4, 'Z')
+    blender_armature = h3d_armature.blender_armature        
+    
+    for f in range(scene.frame_start, scene.frame_end+1):
+        scene.frame_set(f)
+        for i, pbone in enumerate(blender_armature.pose.bones):
+            if not (blender_armature.is_keyframe(f, pbone.path_from_id("location")) or blender_armature.is_keyframe(f, pbone.path_from_id("rotation_axis_angle"))):
+                continue
+            h3d_joint = find_joint_by_name(h3d_armature, pbone.name)
+            keyframe = H3dKeyframe()
+            keyframe.frame = f
+            matrix = base_bone_correction * blender_armature.convert_space(pose_bone=pbone, matrix=pbone.matrix, from_space='POSE', to_space='WORLD') #matrix is absolute?
+            
+            keyframe.position = joint_correction(matrix.to_translation())
+            keyframe.rotation = joint_correction(matrix.to_euler("XZY"))
+            h3d_joint.keyframes.append(keyframe)
+
+def write_armature(f, textual, armature):
+    if textual:
+        f.write("%s\n" % armature.name)
+        f.write("Joint count: %d\n" % len(armature.joints))
+    else:
+        binary_write_string(f, armature.name)
+        f.write(struct.pack("<1i", len(armature.joints)))
+        
+    for joint in armature.joints:
+        if textual:
+            f.write("%s\n" % joint.name)
+            f.write("p %f %f %f\n" % (joint.position[0], joint.position[1], joint.position[2]))
+            f.write("r %f %f %f\n" % (joint.rotation[0], joint.rotation[1], joint.rotation[2]))
+            f.write("< %d\n" % joint.parentIndex)
+            f.write("Keyframes: %d\n" % len(joint.keyframes))
+            for keyframe in joint.keyframes:
+                f.write("f %d\n" % keyframe.frame)
+                f.write("p %f %f %f\n" % (keyframe.position[0], keyframe.position[1], keyframe.position[2]))
+                f.write("r %f %f %f\n" % (keyframe.rotation[0], keyframe.rotation[1], keyframe.rotation[2]))
+
+        else:
+            binary_write_string(f, joint.name)
+            f.write(struct.pack("<3f", *joint.position))
+            f.write(struct.pack("<3f", *joint.rotation))
+            f.write(struct.pack("<1i", joint.parentIndex))
 
 def prepare_armatures(armatures_list):
     base_bone_correction = Matrix.Rotation(pi / 2, 4, 'Z')
@@ -63,9 +128,12 @@ def prepare_armatures(armatures_list):
             parent_bone = bone.parent
             if parent_bone is not None:
                 joint.parentName = parent_bone.name
+                joint.matrix = base_bone_correction * base_matrix * bone.matrix_local #matrix is absolute?
+                """
                 joint.matrix = matrix_difference(
                                     base_matrix * bone.matrix_local,
                                     base_matrix * parent_bone.matrix_local)
+                """
             else:
                 joint.parentName = ""
                 joint.matrix = base_bone_correction * base_matrix * bone.matrix_local
@@ -88,13 +156,15 @@ def find_joint_index(armature, vertex_group):
     except KeyError: #We might get vertex_groups belonging to key shapes (ditch those)
         return -1
     
+def find_joint_by_name(armature, name):
+    return armature.joints[armature.joints_dic[name].index]
 
 def write_some_data(context, filepath, textual):
         
     print("running write_some_data...")
     if textual:
         f = open(filepath, 'w', encoding='utf-8')
-        f.write("H3D V1")
+        f.write("H3D V1\n")
     else:
         f = open(filepath, 'wb')
         f.write(struct.pack("<3c1b", bytes('H', 'ascii'), bytes('3', 'ascii'), bytes('D', 'ascii'), 1))
@@ -131,6 +201,10 @@ def write_some_data(context, filepath, textual):
             armatures.append(armature)
             
     prepare_armatures(armatures)
+    
+    for armature in armatures:
+        fill_keyframes(scene, armature)
+        
     for group in groups:
         if not group.animated:
             continue
@@ -251,7 +325,7 @@ def write_some_data(context, filepath, textual):
                 if texture.image.filepath is not None:
                     texture_image = bpy.path.basename(texture.image.filepath)
         
-        ambient = material.ambient*51 #Hackish to say the least
+        ambient = material.ambient*(51/255) #Hackish to say the least
         
         diffuse = list(material.diffuse_color)
         if(diffuse[0]>0.0 or diffuse[1]>0.0 or diffuse[2]>0.0):
@@ -263,9 +337,9 @@ def write_some_data(context, filepath, textual):
             diffuse = [204/255, 204/255, 204/255]
         
         specular = list(material.specular_color)
-        specular[0] = specular[0]*material.specular_intensity*255
-        specular[1] = specular[1]*material.specular_intensity*255
-        specular[2] = specular[2]*material.specular_intensity*255
+        specular[0] = specular[0]*material.specular_intensity
+        specular[1] = specular[1]*material.specular_intensity
+        specular[2] = specular[2]*material.specular_intensity
         emissive = diffuse[:]
         emissive[0] = emissive[0]*material.emit
         emissive[1] = emissive[1]*material.emit
@@ -297,6 +371,13 @@ def write_some_data(context, filepath, textual):
             f.write(struct.pack("<1f", shininess))
             f.write(struct.pack("<1f", transparency))
         
+    #Write the armatures
+    if textual:
+        f.write("Armatures: %d\n" % len(armatures))
+    else:
+        f.write(struct.pack("<1i", len(armatures)))
+    for armature in armatures:
+        write_armature(f, textual, armature)
     
     f.close()
     
